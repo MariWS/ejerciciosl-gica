932d6602337d75a936c352863528cfb10b0887bd
MariWS <marielactedesco1980@gmail.com>
2023-10-12 16:26:23 -0300

    consola Js Ejercicios de Programación FS PHP

---
+++ script.js
@@ -0,0 +1,98 @@
+/*Unidad 1*/
+
+
+/*Ejerc 1 Nombre y Apellido*/
+var Usuarionombre="Juan" ;
+var Usuarioapellido="Perez" ;
+
+console.log( Usuarionombre + " " + Usuarioapellido)
+
+/*Ejerc 2 Promedio de 3 Notas*/
+
+var nota1= 5;
+var nota2= 7;
+var nota3= 8;
+
+console.log(nota1 + nota2 + nota3)
+
+/*Ejerc 3 Perímetro Triangulo*/
+
+var lado1=5
+var lado2=5
+var lado3=7
+
+console.log(lado1 + lado2 + lado3)
+
+/*Ejerc 4 Perímetro y Área del Cuadrado*/
+
+var cuadradolados= 6
+
+console.log (( cuadradolados * 4 ) + ( lado1 + lado2 + lado3 ))
+
+/*Ejerc 5 Perímetro y Área del Rectángulo*/
+
+var rectladoA=5
+var rectladoB=8
+
+console.log ( "Perímetro Rectángulo=" + " " + (rectladoA*2)+ (rectladoB*2))
+
+var base = 12
+var altura= 8
+
+console.log( "Área Rectángulo=" + " " + (base * altura))
+
+/*Ejerc 6 Resto de Div. Entera entre los valores*/
+
+var numer1=10
+var numer2=3
+
+console.log(10%3)
+
+/*Ejerc 7 msje:"Bienvenido"+ N y A del Usuario*/
+
+var nombreUser= "Mariela"
+var apellidoUser= "Tedesco"
+
+console.log(" Bienvenido" + " " + nombreUser + " " + apellidoUser)
+
+/*Ejerc 8 Perímetro y Área del Círculo*/
+
+var radio= 4
+
+console.log("Perímetro Circulo =" + " "  + (2 * 3.1416 * radio)) 
+
+console.log("Área Circulo =" + " " + 3.1416 * (radio * radio) )
+
+/*Unidad 2*/
+
+/*Ejerc 9 Nombre y Apellido + Bienvenida*/
+
+
+const usernombreserver="Ariel"
+const userapellidoserver="Perez"
+
+var nombreUser1= "Ariel"
+var apellidoUser2= "Perez"
+
+if(usernombreserver == nombreUser1 && userapellidoserver == apellidoUser2){
+
+    console.log( "Bienvenido" + " " + nombreUser1 + " " + apellidoUser2)
+}
+
+/*Ejerc 10 User ingrese Diámetro Circulo calcular Perímetro y área*/
+
+
+
+/*Ejerc 11 user ingrese 4 números sumar y sacar promedio( sumar los 4 y dividir el rdo *4 ese es el promedio)*/
+
+/*Ejerc 12 Sueldos de una Empresa valor por horas - cant de horas trab. por mes y calcular sueldo mensual  */
+
+/*Ejerc 13 Agregar Años de Antiguedad + bono 15% del sueldo por cada año de antiguedad*/
+
+/*Ejerc 14 valor de 1 horas - cant hs trab mes- */
+
+
+/*Unidad 3*/
+
+
+
